7c86a554110f5921a2bf3eb29ee8d6478d9734cc
wswiser101 <59670158+wswiser101@users.noreply.github.com>
2020-08-26 18:47:57 -0500

    begin updating Home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,6 +9,12 @@ export default class Home extends PageBase {
         <h1>Warren Wiser</h1>
         <br/>
         <h5>Welcome to my site!</h5>
+        <div>
+          I created this site after becoming familiar with <code>react</code> during an internship at the Pacific
+          Northwest National Lab in Summer 2020. The repository for this app is&nbsp;
+          <a target={'_blank'} rel={'noopener noreferrer'} href={'//github.com/wswiser101/personal-site'}>here</a>
+          &nbsp;and the readme is below. {/* TODO add readme to this page */}
+        </div>
       </div>
     );
   }
